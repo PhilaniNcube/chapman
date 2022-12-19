bfb04b44066bb3cba6bca323e0ea279d8a0347ad
Philani Ncube <ncbphi001@gmail.com>
2022-12-19 11:38:16 +0200

    family room

--- data.ts
+++ data.ts
@@ -91,7 +91,7 @@ export const rooms = [
     in_season: 2000,
     off_season: 1500,
     breakfast: true,
-    description: "This sea-facing en-suite family room is the size of 2 standard rooms and has two full bathrooms (each with a separate bath and shower)",
+    description: "This sea-facing en-suite family room consists of one double bed and three single beds per room",
     images: ["/images/family_room.jpg", "/images/double_room_2.jpg", "/images/twin_room_3.jpg", "/images/twin_room_4.jpg", "/images/sea_view_10.jpg", "/images/sea_view_8.jpg", "/images/sea_view_9.jpg"]
   },
 ]

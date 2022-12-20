30126b35c179bcc0db776e50c927ccb76adc8f6d
Philani Ncube <ncbphi001@gmail.com>
2022-12-20 16:08:34 +0200

    Christmas menu

--- app/restaurant/specials/page.tsx
+++ app/restaurant/specials/page.tsx
@@ -2,73 +2,93 @@
 import { XataClient } from "../../../utils/xata";
 import { display } from "../../AboutSection";
 
-const xata = new XataClient();
 
-const loadMenu = async () => {
-  const dessert = await xata.db.menu_items
-    .filter({
-      "menu.title": "Desserts",
-    })
-    .getMany();
-
-  const kiddies = await xata.db.menu_items
-    .filter({
-      "menu.title": "Kiddies Meals",
-    })
-    .getMany();
-
-  return {
-    dessert,
-    kiddies
-  };
-};
 
 const page = async () => {
 
-const {dessert, kiddies} = await loadMenu()
+
 
   return (
     <div className="my-6 px-4 lg:px-0">
       <div className="max-w-7xl mx-auto">
         <h1
-          className={`${display.className} text-black text-2xl lg:text-4xl text-center my-6`}
+          className={`${display.className} text-2xl lg:text-4xl font-bold text-center my-4`}
         >
-          Kiddeis Menu
+          Chapmans Christmas Lunch Menu
         </h1>
-        <div className="mt-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
-          {kiddies.map((item, i) => (
-            <div className="w-full py-4 px-3" key={i}>
-              <h3 className="text-lg font-medium border-b border-dashed border-slate-700">
-                {item.title}
-              </h3>
-              <p className="text-sm text-slate-700">{item.description}</p>
-              <h4
-                className={`${display.className} font-bold text-black text-xl`}
-              >
-                R{item.price}
-              </h4>
-            </div>
-          ))}
+
+        <p className="text-xl text-center">25 December 2022</p>
+        <p className="text-xl text-center">First Sitting 12:00pm to 14:00pm</p>
+        <p className="text-xl text-center">Second Sitting 14:30pm to 16:30pm</p>
+
+        <hr className="my-6" />
+
+        <h2 className={`${display.className} text-2xl  text-center my-4`}>
+          Booking Essential
+        </h2>
+        <h2 className={`${display.className} text-2xl  text-center my-4`}>
+          Price Per Adult: R395
+        </h2>
+        <h2 className={`${display.className} text-2xl  text-center my-4`}>
+          Children 10 years &amp; Younger: R395
+        </h2>
+
+        <div className="w-full p-6 lg:p-10 my-4 bg-slate-100 rounded">
+          <h2
+            className={`${display.className} text-2xl  text-center my-4 font-bold uppercase`}
+          >
+            Starters
+          </h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center place-items-center">
+            <p className="text-lg text-slate-800 py-3">
+              Christmas day homemade bread table
+            </p>
+            <p className="text-lg text-slate-800 py-3">Curry Noodle Salad</p>
+            <p className="text-lg text-slate-800 py-3">Greek Salad</p>
+            <p className="text-lg text-slate-800 py-3">
+              Deep fried hake nuggets
+            </p>
+          </div>
         </div>
-        <h1
-          className={`${display.className} text-black text-2xl lg:text-4xl text-center my-6`}
-        >
-          Dessert Menu
-        </h1>
-        <div className="mt-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
-          {dessert.map((item, i) => (
-            <div className="w-full py-4 px-3" key={i}>
-              <h3 className="text-lg font-medium border-b border-dashed border-slate-700">
-                {item.title}
-              </h3>
-              <p className="text-sm text-slate-700">{item.description}</p>
-              <h4
-                className={`${display.className} font-bold text-black text-xl`}
-              >
-                R{item.price}
-              </h4>
-            </div>
-          ))}
+        <div className="w-full p-6 lg:p-10 my-4 bg-slate-100 rounded">
+          <h2
+            className={`${display.className} text-2xl  text-center my-4 font-bold uppercase`}
+          >
+       Main Course
+          </h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center place-items-center">
+            <p className="text-lg text-slate-800 py-3">
+              Sweet &amp; sour port sosaties
+            </p>
+            <p className="text-lg text-slate-800 py-3">Roasted chiken</p>
+            <p className="text-lg text-slate-800 py-3">Boerewors</p>
+            <p className="text-lg text-slate-800 py-3">
+              Creamy mushroom potato bake
+            </p>
+            <p className="text-lg text-slate-800 py-3">
+              Variety of grilled vegetables
+            </p>
+            <p className="text-lg text-slate-800 py-3">
+              Savoury Rice
+            </p>
+          </div>
+        </div>
+        <div className="w-full p-6 lg:p-10 my-4 bg-slate-100 rounded">
+          <h2
+            className={`${display.className} text-2xl  text-center my-4 font-bold uppercase`}
+          >
+      Desserts
+          </h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center place-items-center">
+            <p className="text-lg text-slate-800 py-3">
+              Cheese Cake
+            </p>
+            <p className="text-lg text-slate-800 py-3">Nougat ice-cream</p>
+
+          </div>
         </div>
       </div>
     </div>

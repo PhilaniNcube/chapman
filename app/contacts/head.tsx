5fdcb54771dea4f3e38167a75f4aa44383b216d0
Philani Ncube <ncbphi001@gmail.com>
2022-12-30 17:36:45 +0200

    add sitemap and meta tags

--- app/contacts/head.tsx
+++ app/contacts/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>
-        The Chapman Hotel - A Rich History in Port Elizabeth, South Africa
+        The Chapman Hotel - Contacts
       </title>
       <meta
         name="description"
@@ -13,9 +13,9 @@ export default function Head() {
         content="hotel, history, luxury, Port Elizabeth, South Africa"
       />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="canonical" href="https://www.chapman.co.za/our_history" />
+      <link rel="canonical" href="https://www.chapman.co.za/contacts" />
       <meta name="robots" content="index,follow" />
-      <meta property="og:title" content="The Chapman Hotel - A Rich History" />
+      <meta property="og:title" content="The Chapman Hotel - Contacts" />
       <meta
         property="og:description"
         content="Established in 1998, The Chapman Hotel & Conference Centre is a family business, which is owned and managed by the Franklin family. The Chapman Hotel includes the internationally famous restaurant, Blackbeards® Tavern, which, besides being known as Port Elizabeth's Seafood Landmark, has been a wine and dine destination for the tourism industry since 1975."

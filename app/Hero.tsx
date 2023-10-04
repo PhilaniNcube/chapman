c4ce08e639283372a1b2dec7acb051bd402c7440
Philani Ncube <ncbphi001@gmail.com>
2023-10-04 11:13:12 +0200

    responsive styles for the specials

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -10,51 +10,60 @@ const display = Playfair_Display({
 
 const Hero = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 relative isolate">
-      <div className="h-screen w-full bg-white hidden lg:flex flex-col justify-center items-center"></div>
-      <div className="h-screen w-full bg-black">
+    <div className="relative grid grid-cols-1 lg:grid-cols-2 isolate">
+      <div className="flex-col items-center justify-center hidden w-full h-screen bg-white lg:flex"></div>
+      <div className="w-full h-screen bg-black">
         <Image
           src="/images/pool_1.jpg"
           width={4608}
           height={2592}
           priority
           alt="Pool"
-          className="w-full h-full object-cover"
+          className="object-cover w-full h-full"
         />
       </div>
-      <div
-        className={` absolute inset-0 flex items-center justify-start text-slate-800`}
-      >
-        <div className="max-w-7xl px-4 bg-white/70 lg:bg-transparent pt-36 flex flex-col translate-y-[100px] lg:translate-y-0 justify-center w-full items-start mx-auto">
-          <h1
-            className={`${display.className} text-2xl lg:text-6xl font-semibold text-black`}
-          >
-            Value for money
-            <br /> Accommodation &amp; <br />
-            Conferencing
-          </h1>
-          <p className="mt-8 md:text-md text-sm lg:text-lg text-slate-700 leading-7 max-w-[50ch] font-medium">
-            Situated on Brookes Hill between King&#39;s Beach, the Blue Flag
-            Humewood Beach and the world- renowned water sports mecca of Hobie
-            Beach. The Chapman offers the traveller a blend of well- appointed
-            accommodation and world-class cuisine in a setting reminiscent of
-            lazy summer days of leisure.
-          </p>
+      <div className="absolute inset-0 grid grid-cols-1 lg:grid-cols-2">
+        <div className="h-[60vh] flex items-center justify-start px-10 lg:px-20 text-slate-800">
+          <div className=" px-4 bg-white/70 lg:bg-transparent lg:pt-36 flex py-10 flex-col translate-y-[70px] lg:translate-y-0 justify-center w-full items-start mx-auto">
+            <h1
+              className={`${display.className} text-xl md:text-2xl lg:text-6xl font-semibold text-black`}
+            >
+              Value for money
+              <br /> Accommodation &amp; <br />
+              Conferencing
+            </h1>
+            <p className="mt-3 lg:mt-8 md:text-md text-xs lg:text-lg text-slate-700 lg:leading-7 lg:max-w-[50ch] font-medium">
+              Situated on Brookes Hill between King&#39;s Beach, the Blue Flag
+              Humewood Beach and the world- renowned water sports mecca of Hobie
+              Beach. The Chapman offers the traveller a blend of well- appointed
+              accommodation and world-class cuisine in a setting reminiscent of
+              lazy summer days of leisure.
+            </p>
 
-          <div className="w-full lg:w-1/2 py-3 flex justify-start space-x-4">
-            <Link
-              href="/accommodation"
-              className="bg-black text-white uppercase text-xs md:text-md lg:text-xl w-fit px-8 py-4 mt-5"
-            >
-              Browse Rooms
-            </Link>
-            <Link
-              href="/contacts"
-              className="border-2 border-black text-black uppercase text-xs md:text-md lg:text-xl w-fit px-8 py-4 mt-5"
-            >
-              Contact
-            </Link>
+            <div className="justify-start hidden w-full py-3 space-x-4 text-center lg:flex lg:w-1/2">
+              <Link
+                href="/accommodation"
+                className="px-8 py-4 mt-5 text-xs text-white uppercase bg-black md:text-md lg:text-xl w-fit"
+              >
+                Browse Rooms
+              </Link>
+              <Link
+                href="/contacts"
+                className="px-8 py-4 mt-5 text-xs text-black uppercase border-2 border-black md:text-md lg:text-xl w-fit"
+              >
+                Contact
+              </Link>
+            </div>
           </div>
+        </div>
+        <div className="w-full h-full bg-black">
+          <Image
+            src="/images/specials.jpg"
+            width={900}
+            height={900}
+            alt="Specials"
+            className="object-cover w-full h-full"
+          />
         </div>
       </div>
     </div>

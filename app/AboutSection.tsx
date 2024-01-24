f4759f602c62509967636428f4cd9e5302e4c6ac
Philani Ncube <ncbphi001@gmail.com>
2024-01-24 10:47:29 +0200

    Refactor AboutSection and Hero components

--- app/AboutSection.tsx
+++ app/AboutSection.tsx
@@ -9,18 +9,58 @@ export const display = Playfair_Display({
 });
 
 const AboutSection = () => {
-  return <article className="relative isolate w-full py-20">
-    <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2">
-      <Image src="/images/sea_view_2.jpg" width={4608} height={2592} quality={75} className="w-full h-full hidden lg:flex aspect-square object-cover z-10" alt="View" />
-      <div className="z-20 h-full flex flex-col justify-center items-start bg-transparent relative isolate w-full px-6 py-20">
-         <div className="w-full bg-slate-100 shadow-xl h-full flex flex-col justify-center items-center px-4 py-10 lg:translate-x-[-200px]">
-          <h2 className={`${display.className} text-black text-5xl text-center font-medium`}>Stay with us <br /> Bring the family!</h2>
-          <p className="mt-6 text-slate-700 max-w-[60ch] font-medium text-center">Inspire your senses with the sounds and secrets of the Indian Ocean during your stay in one of The Chapman&apos;s well-aapointed rooms. Find out more about our rooms and what is on offer.</p>
+  return (
+    <article className="relative w-full py-20 isolate">
+      <div className="grid grid-cols-1 mx-auto max-w-7xl lg:grid-cols-2">
+        <Image
+          src="/images/sea_view_2.jpg"
+          width={4608}
+          height={2592}
+          quality={75}
+          className="z-10 hidden object-cover w-full h-full lg:flex aspect-square"
+          alt="View"
+        />
+        <div className="relative z-20 flex flex-col items-start justify-center w-full h-full px-6 py-20 bg-transparent isolate">
+          <div className="w-full bg-slate-100 shadow-xl h-full flex flex-col justify-center items-center px-4 py-10 lg:translate-x-[-200px]">
+            <h2
+              className={`${display.className} text-black text-5xl text-center font-medium`}
+            >
+              Our Rooms
+            </h2>
+            <p className="mt-6 text-slate-700 max-w-[60ch] font-medium text-center">
+              Inspire your senses with the sounds and secrets of the Indian
+              Ocean during your stay in one of The Chapman&apos;s well-aapointed
+              rooms. Find out more about our rooms and what is on offer.
+            </p>
 
-          <Link href="/accommodation" className="border border-black mt-5 w-fit px-8 py-4 uppercase text-black font-medium">Explore Rooms</Link>
-         </div>
+            <ul className="mt-3 text-sm list-disc text-slate-700">
+              <li>
+                En-suite bathroom with a bath, separate shower, hand basin, and
+                toilet
+              </li>
+              <li>
+                Remote-controlled colour television with M-Net and Satellite TV
+              </li>
+              <li>
+                Executive roller top desk with a designer hand-crafted chair
+              </li>
+              <li>Hand-crafted coffee table and two chairs</li>
+              <li>Free-standing Antique style wardrobe</li>
+              <li>Luggage Rack</li>
+              <li>Internet Access</li>
+              <li>Electrical shaving point</li>
+            </ul>
+
+            <Link
+              href="/accommodation"
+              className="px-8 py-4 mt-5 font-medium text-black uppercase border border-black w-fit"
+            >
+              Explore Rooms
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
-  </article>;
+    </article>
+  );
 };
 export default AboutSection;
--- app/Hero.tsx
+++ app/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
       </div>
       <div className="absolute inset-0 grid grid-cols-1 lg:grid-cols-2">
         <div className="h-[60vh] flex items-center justify-start px-10 lg:px-20 text-slate-800">
-          <div className=" px-4 bg-white/70 lg:bg-transparent lg:pt-36 flex py-10 flex-col translate-y-[70px] lg:translate-y-[200px] justify-center w-full items-start mx-auto">
+          <div className=" px-4 bg-white/70 lg:bg-transparent lg:pt-36 flex py-10 flex-col translate-y-[140px] lg:translate-y-[200px] justify-center w-full items-start mx-auto">
             <h1
               className={`${display.className} text-xl md:text-2xl lg:text-6xl font-semibold text-black`}
             >
@@ -33,23 +33,27 @@ const Hero = () => {
               Conferencing
             </h1>
             <p className="mt-3 lg:mt-8 md:text-md text-xs lg:text-lg text-slate-700 lg:leading-7 lg:max-w-[50ch] font-medium">
-              Situated on Brookes Hill between King&#39;s Beach, the Blue Flag
-              Humewood Beach and the world- renowned water sports mecca of Hobie
-              Beach. The Chapman offers the traveller a blend of well- appointed
-              accommodation and world-class cuisine in a setting reminiscent of
-              lazy summer days of leisure.
+              The Chapman is a stylish boutique hotel in Port Elizabeth,
+              offering 23 up-market rooms with stunning sea views. The hotel is
+              located near the Port Elizabeth Oceanarium and is designed
+              according to four-star standards. All rooms are en-suite and face
+              the ocean, with upstairs rooms featuring balconies and air
+              conditioning, and downstairs rooms opening onto the garden and
+              pool area. The rooms are elegantly furnished with classic
+              finishes, providing a luxurious and comfortable stay while
+              showcasing the breathtaking views of Algoa Bay.
             </p>
 
             <div className="justify-start hidden w-full py-3 space-x-4 text-center lg:flex lg:w-2/3">
               <Link
                 href="/accommodation"
-                className="px-2 xl:px-8 py-4 mt-5 text-xs text-white uppercase bg-black md:text-md lg:text-md  "
+                className="px-2 py-4 mt-5 text-xs text-white uppercase bg-black xl:px-8 md:text-md lg:text-md "
               >
                 Browse Rooms
               </Link>
               <Link
                 href="/contacts"
-                className="px-2 xl:px-8 py-4 mt-5 text-xs text-black uppercase border-2 border-black md:text-md lg:text-md "
+                className="px-2 py-4 mt-5 text-xs text-black uppercase border-2 border-black xl:px-8 md:text-md lg:text-md "
               >
                 Contact
               </Link>

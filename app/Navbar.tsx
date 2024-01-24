3bf25b4336fe4209f3c6102934a6239f9e9b219c
Philani Ncube <ncbphi001@gmail.com>
2024-01-24 10:59:56 +0200

    Add new dependencies and update styles

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,24 +1,73 @@
-import { link } from "fs";
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import { links } from "../data";
 import analytics from "../utils/analytics";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../components/ui/sheet";
+import { MenuIcon } from "lucide-react";
+
 
 const Navbar = () => {
 
     analytics.page();
 
-  return <header className="px-4 absolute top-0 left-0 right-0 py-6 z-50">
-    {/** Desktop Menu **/}
-     <nav className="max-w-7xl mx-auto px-3 lg:px-8 shadow-lg py-1 bg-white/70 flex items-center justify-between">
-       <Image src="/images/logo.jpg" width={227} height={193} alt="logo" priority className="w-32 lg:w-24 object-cover" />
+  return (
+    <header className="absolute top-0 left-0 right-0 z-50 px-4 py-6">
+      {/** Desktop Menu **/}
+      <nav className="flex items-center justify-between px-3 py-1 mx-auto shadow-lg max-w-7xl lg:px-8 bg-white/70">
+        <Image
+          src="/images/logo.jpg"
+          width={227}
+          height={193}
+          alt="logo"
+          priority
+          className="object-cover w-32 lg:w-24"
+        />
 
-       <div className="hidden md:flex gap-2 space-x-3 items-center">
-        {links.map((link) => (
-          <Link href={link.url} key={link.id} className="text-gray-800 uppercase text-md font-medium" >{link.text}</Link>
-        ))}
-       </div>
-     </nav>
-  </header>;
+        <div className="items-center hidden gap-2 space-x-3 md:flex">
+          {links.map((link) => (
+            <Link
+              href={link.url}
+              key={link.id}
+              className="font-medium text-gray-800 uppercase text-md"
+            >
+              {link.text}
+            </Link>
+          ))}
+        </div>
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <MenuIcon className="w-6 h-6 text-gray-800" />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+
+              </SheetHeader>
+              <div className="flex flex-col mt-3 space-y-3">
+                {links.map((link) => (
+                  <Link
+                    href={link.url}
+                    key={link.id}
+                    className="font-medium text-gray-800 uppercase text-md"
+                  >
+                    {link.text}
+                  </Link>
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </nav>
+    </header>
+  );
 };
 export default Navbar;

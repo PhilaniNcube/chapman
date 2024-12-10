bc7551a9fc791a03d3b5c79162a9441ea9f0f0bd
Philani Ncube <ncbphi001@gmail.com>
2024-12-10 12:25:20 +0200

    Update Christmas Day Lunch menu for 2024 with new pricing and details

--- app/restaurant/specials/page.tsx
+++ app/restaurant/specials/page.tsx
@@ -1,4 +1,5 @@
 
+import Link from "next/link";
 import { XataClient } from "../../../utils/xata";
 import { display } from "../../AboutSection";
 
@@ -9,15 +10,15 @@ const page = async () => {
 
 
   return (
-    <div className="my-6 px-4 lg:px-0">
-      <div className="max-w-7xl mx-auto">
+    <div className="px-4 my-6 lg:px-0">
+      <div className="mx-auto max-w-7xl">
         <h1
           className={`${display.className} text-2xl lg:text-4xl font-bold text-center my-4`}
         >
-          Chapmans Christmas Lunch Menu
+          CHRISTMAS DAY LUNCH 2024
         </h1>
 
-        <p className="text-xl text-center">25 December 2022</p>
+        <p className="text-xl text-center">25 December 2024</p>
         <p className="text-xl text-center">First Sitting 12:00pm to 14:00pm</p>
         <p className="text-xl text-center">Second Sitting 14:30pm to 16:30pm</p>
 
@@ -27,68 +28,76 @@ const page = async () => {
           Booking Essential
         </h2>
         <h2 className={`${display.className} text-2xl  text-center my-4`}>
-          Price Per Adult: R395
+          Price Per Adult: R495
         </h2>
         <h2 className={`${display.className} text-2xl  text-center my-4`}>
-          Children 10 years &amp; Younger: R275
+          Children 10 years &amp; Younger: R325
         </h2>
 
-        <div className="w-full p-6 lg:p-10 my-4 bg-slate-100 rounded">
+        <div className="w-full p-6 my-4 rounded lg:p-10 bg-slate-100">
           <h2
             className={`${display.className} text-2xl  text-center my-4 font-bold uppercase`}
           >
             Starters
           </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 items-center place-items-center">
-            <p className="text-lg text-slate-800 py-3">
+          <div className="grid items-center grid-cols-1 md:grid-cols-2 place-items-center">
+            <p className="py-3 text-lg text-slate-800">
               Christmas day homemade bread table
             </p>
-            <p className="text-lg text-slate-800 py-3">Curry Noodle Salad</p>
-            <p className="text-lg text-slate-800 py-3">Greek Salad</p>
-            <p className="text-lg text-slate-800 py-3">
+            <p className="py-3 text-lg text-slate-800">
+              Homemade Garlic & Feta Breads
+            </p>
+            <p className="py-3 text-lg text-slate-800">Curry Noodle Salad</p>
+            <p className="py-3 text-lg text-slate-800">Greek Salad</p>
+            <p className="py-3 text-lg text-slate-800">
               Deep fried hake nuggets
             </p>
           </div>
         </div>
-        <div className="w-full p-6 lg:p-10 my-4 bg-slate-100 rounded">
+        <div className="w-full p-6 my-4 rounded lg:p-10 bg-slate-100">
           <h2
             className={`${display.className} text-2xl  text-center my-4 font-bold uppercase`}
           >
-       Main Course
+            Main Course
           </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 items-center place-items-center">
-            <p className="text-lg text-slate-800 py-3">
+          <div className="grid items-center grid-cols-1 md:grid-cols-2 place-items-center">
+            <p className="py-3 text-lg text-slate-800">
               Sweet &amp; sour pork sosaties
             </p>
-            <p className="text-lg text-slate-800 py-3">Roasted chicken</p>
-            <p className="text-lg text-slate-800 py-3">Boerewors</p>
-            <p className="text-lg text-slate-800 py-3">
-              Creamy mushroom potato bake
-            </p>
-            <p className="text-lg text-slate-800 py-3">
+            <p className="py-3 text-lg text-slate-800">Roasted chicken</p>
+            <p className="py-3 text-lg text-slate-800">Boerewors</p>
+            <p className="py-3 text-lg text-slate-800">Seafood potato bake</p>
+            <p className="py-3 text-lg text-slate-800">
               Variety of grilled vegetables
             </p>
-            <p className="text-lg text-slate-800 py-3">
-              Savoury Rice
-            </p>
+            <p className="py-3 text-lg text-slate-800">Savoury Rice</p>
           </div>
         </div>
-        <div className="w-full p-6 lg:p-10 my-4 bg-slate-100 rounded">
+        <div className="w-full p-6 my-4 rounded lg:p-10 bg-slate-100">
           <h2
             className={`${display.className} text-2xl  text-center my-4 font-bold uppercase`}
           >
-      Desserts
+            Desserts
           </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 items-center place-items-center">
-            <p className="text-lg text-slate-800 py-3">
-              Cheese Cake
-            </p>
-            <p className="text-lg text-slate-800 py-3">Nougat ice-cream</p>
-
+          <div className="grid items-center grid-cols-1 md:grid-cols-2 place-items-center">
+            <p className="py-3 text-lg text-slate-800">Cheese Cake</p>
+            <p className="py-3 text-lg text-slate-800">Nougat ice-cream</p>
           </div>
+          <p className="py-3 text-lg text-slate-800">
+            Please note: The prices above do not include a service gratuity.
+            Prepayment is a prerequisite. Kindly note that the above excludes a
+            standard 10% service fee which will need to be paid on the day.
+          </p>
+          <h3>The Chapman Hotel & Conference Centre</h3>
+          <p>
+            Telephone: 041 5840678
+          </p>
+            <Link href="mailto:chapmail@iafrica.com">
+              Email: chapmail@iafrica.com
+            </Link>
         </div>
       </div>
     </div>
